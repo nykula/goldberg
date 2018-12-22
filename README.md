f56cec76329222d32f80caa068df88e2ca4278cc
makepost <makepost@firemail.cc>
2018-12-22 01:13:57 +0000

    Nav: Tab

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # goldberg
 
-GTK+ frame for WebKit, in other words a Gjs web browser. 
+GTK+ frame for WebKit, in other words a web browser.
+
+## Usage
+
+```sh
+USE='introspection jit' emerge '>=gjs-1.55' '>=webkit-gtk-2.23'
+git clone https://github.com/makepost/goldberg
+cd goldberg
+./index.js
+```
+
+For development, `npm i` installs type checker, `npm run format` runs it.
 
 ## License
 
--- index.js
+++ index.js
@@ -2,92 +2,102 @@
 const {
   AccelFlags,
   AccelGroup,
+  Application,
+  ApplicationWindow,
+  Box,
   Button,
   Entry,
   HeaderBar,
   IconSize,
   Image,
+  Label,
+  Notebook,
+  PackType,
   ReliefStyle,
-  Window,
   WindowPosition
 } = imports.gi.Gtk;
-const { WebView } = imports.gi.WebKit2;
+const { EllipsizeMode } = imports.gi.Pango;
+const { ProcessModel, WebContext, WebView } = imports.gi.WebKit2;
 
-imports.gi.Gtk.init(null);
-/** @type {any} */ const REF = undefined;
+/** @param {any} [x] */ const ANY = x => x;
 const state = {
-  /** @type {Button} */ $next: REF,
-  /** @type {Button} */ $previous: REF,
-  /** @type {Entry} */ $url: REF,
-  /** @type {WebView} */ $web: REF
+  /** @type {Notebook} */ $tabs: ANY(),
+  /** @type {Entry} */ $url: ANY(),
+  /** @type {WebView} */ $web: ANY(),
+  /** @type {{ $: WebView, _: (() => void)[] }} */ _$web: { $: ANY(), _: [] }
 };
-const $win = new Window({
-  default_height: 600,
-  default_width: 1024,
-  title: "Goldberg",
-  window_position: WindowPosition.CENTER
+Object.defineProperty(state, "$web", {
+  get: () => state._$web.$,
+  set(/** @type {typeof state._$web.$} */ x) {
+    state._$web.$ = x;
+    state._$web._.map(y => y());
+  }
 });
-$win.connect(
-  "destroy",
-  () => imports.gi.Gtk.main_quit()
-);
-{
-  const $accel = new AccelGroup();
-  /** @param {string} xs @param {() => void} callback */
-  const accel = (xs, callback) =>
-    xs.split(";").map(x => {
-      const [key, mod] = imports.gi.Gtk.accelerator_parse(x).map(Number);
-      $accel.connect(
-        key,
-        mod,
-        AccelFlags.VISIBLE,
-        callback
-      );
-    });
-  accel("<Ctrl>l;<Meta>l;F6", () => (state.$url.grab_focus(), true));
-  accel("<Ctrl>r;F5", () => state.$web.reload());
-  accel("<Meta>l", () => (state.$url.grab_focus(), true));
-  accel("<Meta>Left", () => state.$web.go_back());
-  accel("<Meta>Right", () => state.$web.go_forward());
-  $win.add_accel_group($accel);
-
-  const $nav = new HeaderBar({ show_close_button: true });
+/** @type {<T>(x: { connect: T }) => T} */
+const on = x => ANY((/** @type {any[]} */ ...args) => x.connect(...args));
+/** @param {{ icon_name: string }} props */
+const ButtonNav = ({ icon_name }) =>
+  new Button({
+    focus_on_click: false,
+    image: new Image({ icon_name, icon_size: IconSize.MENU }),
+    relief: ReliefStyle.NONE
+  });
+const $app = new Application({ application_id: "org.js.goldberg" });
+on($app)("activate", () => {
+  WebContext.get_default().set_process_model(
+    ProcessModel.MULTIPLE_SECONDARY_PROCESSES
+  );
+  const $win = new ApplicationWindow({
+    application: $app,
+    default_height: 600,
+    default_width: 1024,
+    title: "Goldberg",
+    window_position: WindowPosition.CENTER
+  });
   {
-    /** @param {{ icon_name: string }} props */
-    const NavButton = ({ icon_name }) =>
-      new Button({
-        focus_on_click: false,
-        image: new Image({ icon_name, icon_size: IconSize.MENU }),
-        relief: ReliefStyle.NONE
+    const $accel = new AccelGroup();
+    /** @param {string} xs @param {() => void} callback */
+    const accel = (xs, callback) =>
+      xs.split(";").map(x => {
+        const [key, mod] = imports.gi.Gtk.accelerator_parse(x).map(Number);
+        on($accel)(key, mod, AccelFlags.VISIBLE, () => (callback(), true));
       });
-    const $previous = NavButton({ icon_name: "go-previous-symbolic" });
-    $previous.connect(
-      "clicked",
-      () => state.$web.go_back()
-    );
-    state.$previous = $previous;
-    $nav.add($previous);
+    accel("<Ctrl>l;<Meta>l;F6", () => state.$url.grab_focus());
+    accel("<Ctrl>Page_Down", () => state.$tabs.next_page());
+    accel("<Ctrl>Page_Up", () => state.$tabs.prev_page());
+    accel("<Ctrl>r;F5", () => state.$web.reload());
+    accel("<Ctrl>t", () => Blank());
+    accel("<Ctrl>w", () => state.$web.destroy());
+    accel("<Meta>Left", () => state.$web.go_back());
+    accel("<Meta>Right", () => state.$web.go_forward());
+    accel("Escape", () => state.$web.stop_loading());
+    $win.add_accel_group($accel);
+  }
+  {
+    const $nav = new HeaderBar({ show_close_button: true });
 
-    const $next = NavButton({ icon_name: "go-next-symbolic" });
-    $next.connect(
-      "clicked",
-      () => state.$web.go_forward()
-    );
-    state.$next = $next;
+    const $prev = ButtonNav({ icon_name: "go-previous-symbolic" });
+    on($prev)("clicked", () => state.$web.go_back());
+    state._$web._.push(() => ($prev.sensitive = state.$web.can_go_back()));
+    $nav.add($prev);
+
+    const $next = ButtonNav({ icon_name: "go-next-symbolic" });
+    on($next)("clicked", () => state.$web.go_forward());
+    state._$web._.push(() => ($next.sensitive = state.$web.can_go_forward()));
     $nav.add($next);
 
-    const $refresh = NavButton({ icon_name: "view-refresh-symbolic" });
-    $refresh.connect(
-      "clicked",
-      () => state.$web.reload()
-    );
+    const $refresh = ButtonNav({ icon_name: "view-refresh-symbolic" });
+    on($refresh)("clicked", () => state.$web.reload());
+    state._$web._.push(() => ($refresh.visible = !state.$web.is_loading));
     $nav.add($refresh);
-  }
 
-  const $url = new Entry({ expand: true });
-  $url.connect(
-    "activate",
-    () => {
+    const $stop = ButtonNav({ icon_name: "process-stop-symbolic" });
+    on($stop)("clicked", () => state.$web.stop_loading());
+    state._$web._.push(() => ($stop.visible = state.$web.is_loading));
+    $nav.add($stop);
+
+    const $url = new Entry({ expand: true });
+    on($url)("activate", () => {
       const x = $url.text;
       state.$web.grab_focus();
       state.$web.load_uri(
@@ -97,32 +107,62 @@ $win.connect(
           ? `https://${x}`
           : x
       );
-    }
-  );
-  $url.connect(
-    "focus-out-event",
-    () => $url.select_region(0, 0)
-  );
-  state.$url = $url;
-  $nav.custom_title = $url;
-  $win.set_titlebar($nav);
+    });
+    on($url)("focus-out-event", () => $url.select_region(0, 0));
+    state._$web._.push(() => ($url.text = decodeURI(state.$web.uri || "")));
+    $nav.custom_title = state.$url = $url;
 
-  const $web = new WebView();
-  $web.get_settings().enable_developer_extras = true;
-  $web.connect(
-    "load-changed",
-    () => {
-      state.$previous.sensitive = $web.can_go_back();
-      state.$next.sensitive = $web.can_go_forward();
-      state.$url.text = $web.get_uri();
-    }
-  );
-  state.$web = $web;
-  $win.add(/** @type {any} */ ($web));
-}
-$win.show_all();
-state.$web.load_uri("https://example.com/");
+    const $add = ButtonNav({ icon_name: "list-add-symbolic" });
+    on($add)("clicked", () => Blank());
+    state._$web._.push(() => ($add.visible = state.$tabs.get_n_pages() <= 1));
+    $nav.pack_end($add);
 
-imports.gi.Gtk.main();
-/** @type {any} */ (window).exports = {};
+    $win.set_titlebar($nav);
+  }
+  {
+    const $tabs = new Notebook({ scrollable: true, show_border: false });
+    on($tabs)("switch-page", (_, $web) => (state.$web = ANY($web)));
+    state._$web._.push(() => ($tabs.show_tabs = $tabs.get_n_pages() > 1));
+    const $add = ButtonNav({ icon_name: "list-add-symbolic" });
+    on($add)("clicked", () => Blank());
+    $add.margin_right = 5;
+    $add.visible = true;
+    $tabs.set_action_widget($add, PackType.END);
+    $win.add((state.$tabs = $tabs));
+  }
+  $win.show_all();
+  const Blank = () => (Current(Tab()), state.$url.grab_focus());
+  /** @param {WebView} $web */
+  const Current = $web => {
+    $web.visible = true;
+    on($web)("destroy", () => state.$tabs.get_n_pages() || $win.destroy());
+    const $box = new Box();
+    const $label = new Label({ ellipsize: EllipsizeMode.END, hexpand: true });
+    on($web)("notify::title", () => ($label.label = $web.title || $web.uri));
+    $box.add(Object.assign($label, { label: "..." }));
+    const $close = ButtonNav({ icon_name: "window-close-symbolic" });
+    on($close)("clicked", () => $web.destroy());
+    $box.add($close);
+    $box.show_all();
+    const i = state.$tabs.append_page(ANY($web), $box);
+    state.$tabs.set_current_page(i);
+    return $web;
+  };
+  const Tab = () => {
+    const $web = new WebView();
+    on($web)("create", Tab);
+    on($web)("load-changed", () => state.$web === $web && (state.$web = $web));
+    on($web)(
+      "mouse-target-changed",
+      (_, x) => (state.$url.text = x.get_link_uri() || _.uri || state.$url.text)
+    );
+    on($web)("notify::uri", () => state.$web === $web && (state.$web = $web));
+    on($web)("ready-to-show", Current);
+    $web.get_settings().enable_developer_extras = true;
+    return $web;
+  };
+  Blank();
+});
+$app.run(ARGV);
+ANY(window).exports = {};
 exports.forceTsNoUnusedLocals = "random property on exports";
